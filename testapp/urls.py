6f72f5996b7c66084bd574b71160696cb504dfbb
phyllers <plee@systemsbiology.org>
2014-12-02 17:23:08 -0800

    Removed ignored files, added gitignore, hopefully fixed testapp/urls

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('testapp.views',
     (r'^$', 'list_greetings'),
-    (r'^sign$', 'create_greeting')
+    (r'^sign/$', 'create_greeting')
 )
